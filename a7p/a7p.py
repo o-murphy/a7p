0e81a8cfca97bf2d6fbebb2f160709d3f5b142ff
Murphy <thehelixpg@gmail.com>
2023-08-30 19:29:24 +0300

    init commit

---
+++ a7p/a7p.py
@@ -0,0 +1,67 @@
+import hashlib
+import json
+from typing import BinaryIO
+
+from a7p import profedit_pb2
+from google.protobuf.json_format import MessageToJson, MessageToDict, Parse
+
+__all_ = ['A7PFile', 'A7PDataError']
+
+
+class A7PDataError(Exception):
+    pass
+
+
+class A7PFile:
+
+    @staticmethod
+    def loads(string: bytes):
+        data = string[32:]
+        md5_hash = hashlib.md5(data).hexdigest()
+        if md5_hash == string[:32].decode():
+            profile = profedit_pb2.Payload()
+            profile.ParseFromString(data)
+            return profile
+        else:
+            raise A7PDataError("Input data is missing for MD5 hashing")
+
+    @staticmethod
+    def load(file: BinaryIO) -> profedit_pb2.Payload:
+        string = file.read()
+        return A7PFile.loads(string)
+
+    @staticmethod
+    def dumps(profile: profedit_pb2.Payload) -> bytes:
+        data = profile.SerializeToString()
+        md5_hash = hashlib.md5(data).hexdigest().encode()
+        return md5_hash + data
+
+    @staticmethod
+    def dump(profile: profedit_pb2.Payload, file: BinaryIO) -> None:
+        data = A7PFile.dumps(profile)
+        file.write(data)
+
+    @staticmethod
+    def to_json(profile: profedit_pb2.Payload) -> str:
+        return MessageToJson(profile)
+
+    @staticmethod
+    def from_json(json_data: str) -> profedit_pb2.Payload:
+        return Parse(json_data, profedit_pb2.Payload())
+
+    @staticmethod
+    def to_dict(profile: profedit_pb2.Payload) -> dict:
+        return MessageToDict(profile)
+
+    @staticmethod
+    def from_dict(data: dict) -> profedit_pb2.Payload:
+        return Parse(json.dumps(data), profedit_pb2.Payload())
+
+
+if __name__ == '__main__':
+    with open('test.a7p', 'rb') as a7p:
+        a7p_obj = A7PFile.load(a7p)
+        a7p_dict = A7PFile.to_dict(a7p_obj)
+        a7p_json = A7PFile.to_json(a7p_obj)
+        a7p_by_dict = A7PFile.from_dict(a7p_dict)
+        a7p_by_json = A7PFile.from_json(a7p_json)
---
+++ setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages
+
+from a7p import __version__, __author__
+
+
+CLASSIFIERS = [
+    'Intended Audience :: Developers',
+    'Natural Language :: English',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+]
+
+KEYWORDS = 'protobuf, archer, a7p'
+
+
+with open('requirements.txt', 'r') as fp:
+    setup_requires = fp.readlines()
+
+
+with open('README.md', 'r') as fp:
+    long_description = fp.read()
+
+
+package_data = {'a7p': ['requirements.txt', 'a7p/test.a7p']}
+
+setup(
+    name='a7p',
+    version=__version__,
+    python_requires='>=3.9',
+
+    description='pure python fork of dfu_util library',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    author=__author__,
+    url='https://github.com/o-murphy/a7p_transfer_example',
+    download_url='https://github.com/o-murphy/a7p_transfer_example',
+
+    classifiers=CLASSIFIERS,
+    keywords=KEYWORDS,
+
+    packages=find_packages(),
+    install_requires=setup_requires,
+    py_modules=['a7p'],
+
+    include_package_data=True,
+    package_data=package_data,
+
+    zip_safe=False,
+)
+
