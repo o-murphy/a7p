d3656da4f206400863438200353b925ce03b8351
Dmytro Yaroshenko <73843436+o-murphy@users.noreply.github.com>
2023-11-07 08:59:37 +0200

    Dependencies fix (#7)
    * dependencies fix
    * makes validation optional

--- a7p/__init__.py
+++ a7p/__init__.py
@@ -1,10 +1,11 @@
-__version__ = '0.0.7'
+__version__ = '0.0.8'
 __author__ = "o-murphy"
 __credits__ = ["Dmytro Yaroshenko"]
 __copyright__ = ("",)
 
 try:
     from a7p import profedit_pb2, factory
+    from a7p import protovalidate as validator
     from a7p.a7p import A7PFile, A7PDataError
     from a7p.profedit_pb2 import *
     from a7p.factory import A7PFactory
