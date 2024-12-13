6bfed981e9eabc77d5d7386a7f618addcf393068
Dmytro Yaroshenko <73843436+o-murphy@users.noreply.github.com>
2024-12-13 12:56:58 +0200

    distances editing added

--- a7p/__main__.py
+++ a7p/__main__.py
@@ -2,12 +2,13 @@ import asyncio
 import pathlib
 import sys
 from argparse import ArgumentParser
+from asyncio import Semaphore
 from dataclasses import dataclass
 from importlib import metadata
-from asyncio import Semaphore
 
-from a7p.a7p import A7PFile, A7PDataError
+from a7p import A7PFile, A7PDataError
 from a7p import protovalidate
+from a7p.factory import DistanceTable
 from a7p.protovalidate import ValidationError, Violations
 
 try:
@@ -43,17 +44,24 @@ parser.add_argument('-V', '--version', action='version', version=__version__)
 parser.add_argument('-r', '--recursive', action='store_true', help="Recursively walk files")
 parser.add_argument('-v', '--validate', action='store_true', help="Validate files")
 parser.add_argument('--verbose', action='store_true', help="Verbose")
-
-# parser.add_argument('-d', '--distances', action='store', help="Set distances set",
-#                     choices=['subsonic', 'medium', 'long', 'ultra'])
-# parser.add_argument('-zs', '--zero-sync', action='store_true', help="Synchronize zero")
-# parser.add_argument('-zo', '--zero-offset', action='store', nargs=2, help="Set clicks offset",
-#                     metavar=("X_OFFSET", "Y_OFFSET"))
-# parser.add_argument('-cs', '--clicks-switch', action='store', nargs=4, help="Switch clicks sizes",
-#                     metavar=("CUR_X", "NEW_X", "CUR_Y", "NEW_Y"))
-#
+parser.add_argument('-F', '--force', action='store_true', help="Force changes saving")
 # parser.add_argument('--json', action='store', type=pathlib.Path, help="Convert to/from JSON")
-#
+
+distances_group = parser.add_argument_group("Distances")
+distances_group.add_argument('-zd', '--zero-distance', action='store', help="Set zero distance in meters",
+                    type=int)
+distances_group.add_argument('-d', '--distances', action='store', help="Set distances range",
+                    choices=['subsonic', 'low', 'medium', 'long', 'ultra'])
+
+# group = parser.add_argument_group("Zeroing")
+
+
+# group.add_argument('-zs', '--zero-sync', action='store_true', help="Synchronize zero")
+# group.add_argument('-zo', '--zero-offset', action='store', nargs=2, help="Set clicks offset",
+#                     metavar=("X_OFFSET", "Y_OFFSET"))
+# group.add_argument('-cs', '--clicks-switch', action='store', nargs=4, help="Switch clicks sizes",
+#                     metavar=("CUR_X", "NEW_X", "CUR_Y", "NEW_Y"))
+
 # parser.add_argument('--max-threads', action='store', type=int, default=5)
 
 
@@ -63,6 +71,9 @@ class Result:
     error = None
     violations: Violations = None
     zero: tuple[float, float] = None
+    distances: str = None
+    zero_distance: str = None
+    payload: object = None
 
     def print(self):
         print(f"File: {self.path.absolute()}")
@@ -70,6 +81,12 @@ class Result:
             print("Zero: X: {}, Y: {}".format(*self.zero))
         if self.error:
             print(f"Error: {self.error}")
+
+        if self.distances:
+            print("New range: {}".format(self.distances))
+
+        if self.zero_distance:
+            print("New zero distance: {}".format(self.zero_distance))
 
     def details(self):
 
@@ -88,11 +105,60 @@ class Result:
                 except Exception:
                     print(violation)
 
+    def save_changes(self):
+        if self.zero_distance or self.distances:
+            try:
+                with open(self.path.absolute(), 'wb') as fp:
+                    try:
+                        A7PFile.dump(self.payload, fp, validate=True)
+                    except ValidationError:
+                        print("Invalid data, changes would not be saved")
+            except IOError as e:
+                print("Error while saving")
 
-def process_file(path, validate=False, distances=None):
+
+DISTANCES = {
+    'subsonic': DistanceTable.SUBSONIC.value,
+    'low': DistanceTable.LOW_RANGE.value,
+    'medium': DistanceTable.MEDIUM_RANGE.value,
+    'long': DistanceTable.LONG_RANGE.value,
+    'ultra': DistanceTable.ULTRA_RANGE.value
+}
+
+
+def update_distances(payload, distances, zero_distance):
+    if not zero_distance:
+        cur_zero_distance = payload.profile.distances[payload.profile.c_zero_distance_idx]
+    else:
+        cur_zero_distance = (zero_distance * 100)
+    if distances:
+        new_distances = [int(d * 100) for d in DISTANCES[distances]]
+    else:
+        new_distances = payload.profile.distances[:]
+    new_distances.append(cur_zero_distance)
+    new_distances = list(set(new_distances))
+    new_distances.sort()
+    payload.profile.distances[:] = new_distances
+    payload.profile.c_zero_distance_idx = new_distances.index(cur_zero_distance)
+
+
+def update_data(payload, distances, zero_distance):
+    update_distances(payload, distances, zero_distance)
+
+
+def process_file(
+        path,
+        validate=False,
+        distances=None,
+        zero_distance=None
+):
     if path.suffix != ".a7p":
         return
-    result = Result(path)
+    result = Result(
+        path,
+        distances=distances,
+        zero_distance=zero_distance
+    )
     try:
         with open(path, 'rb') as fp:
             data = fp.read()
@@ -107,7 +173,12 @@ def process_file(path, validate=False, distances=None):
     except (IOError, A7PDataError) as e:
         result.error = e
         return result
+
+    if distances or zero_distance:
+        update_data(payload, distances, zero_distance)
+
     result.zero = (payload.profile.zero_x / 1000, payload.profile.zero_y / 1000)
+    result.payload = payload
     return result
 
 
@@ -116,13 +187,15 @@ async def process_files(
         recursive: bool = False,
         validate: bool = False,
         distances: str = None,
+        zero_distance: int = None,
         json: pathlib.Path = None,
         verbose: bool = False,
+        force: bool = False,
 ):
     tasks = []
 
     if not path.is_dir():
-        tasks.append(asyncio.to_thread(process_file, path, validate, distances))
+        tasks.append(asyncio.to_thread(process_file, path, validate, distances, zero_distance))
     else:
         if json is not None:
             parser.error("--json conversion available only for a single file")
@@ -130,20 +203,27 @@ async def process_files(
             item: pathlib.Path
             for item in path.rglob("*"):  # '*' matches all files and directories
                 if item.is_file():
-                    tasks.append(limited_to_thread(process_file, item, validate, distances))
+                    tasks.append(limited_to_thread(process_file, item, validate, distances, zero_distance))
         else:
             for item in path.iterdir():
                 if item.is_file():
-                    tasks.append(limited_to_thread(process_file, item, validate, distances))
+                    tasks.append(limited_to_thread(process_file, item, validate, distances, zero_distance))
 
-    results: list[Result] = await asyncio.gather(*tasks)
+    results: tuple[Result] = await asyncio.gather(*tasks)
 
     for result in results:
         if result:
             result.print()
             if verbose:
                 result.details()
-            print()
+            if force:
+                result.save_changes()
+            else:
+                yes_no = input("Are you sure you want to save changes? Y/N: ")
+                if yes_no.lower() == "y":
+                    result.save_changes()
+                else:
+                    print("Changes would not be saved")
 
 
 def main():
