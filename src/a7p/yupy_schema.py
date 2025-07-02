c2246aeecb1b4172b95375457823e043a84101db
Dmytro Yaroshenko <73843436+o-murphy@users.noreply.github.com>
2025-07-02 16:00:04 +0300

    yupy-0.2 test

--- src/a7p/yupy_schema.py
+++ src/a7p/yupy_schema.py
@@ -1,5 +1,5 @@
 from google.protobuf.json_format import MessageToDict
-from yupy import mapping, array, string, number, mixed, ValidationError, Constraint
+from yupy import mapping, array, string, number, mixed, ValidationError, Constraint, required
 
 from a7p import profedit_pb2
 
@@ -10,57 +10,57 @@ __all__ = (
 _schema = mapping().shape({
     'profile': mapping().shape({
         # descriptor
-        'profile_name': string().max(50).required('Profile name is required'),
-        'cartridge_name': string().max(50).required('Cartridge name is required'),
-        'bullet_name': string().max(50).required('Bullet name is required'),
-        'short_name_top': string().max(8).required('Short name top is required'),
-        'short_name_bot': string().max(8).required('Short name bottom is required'),
-        'caliber': string().max(50).required('Caliber is required'),
-        'device_uuid': string().max(50).not_required(),
-        'user_note': string().max(1024).not_required(),
+        'profile_name': required(string().max(50), 'Profile name is required'),
+        'cartridge_name': required(string().max(50), 'Cartridge name is required'),
+        'bullet_name': required(string().max(50), 'Bullet name is required'),
+        'short_name_top': required(string().max(8), 'Short name top is required'),
+        'short_name_bot': required(string().max(8), 'Short name bottom is required'),
+        'caliber': required(string().max(50), 'Caliber is required'),
+        'device_uuid': string().max(50),
+        'user_note': string().max(1024),
 
         # zeroing
-        'zero_x': number().ge(-200000).le(200000).integer().required('Zero X is required'),
-        'zero_y': number().ge(-200000).le(200000).integer().required('Zero Y is required'),
+        'zero_x': required(number().ge(-200000).le(200000).integer(), 'Zero X is required'),
+        'zero_y': required(number().ge(-200000).le(200000).integer(), 'Zero Y is required'),
 
         # lists
-        'distances': array().of(number().ge(100).le(300000).integer().required()).min(1).max(200),
+        'distances': required(array().of(number().ge(100).le(300000).integer()).min(1).max(200)),
         'switches': array().of(
             mapping().shape({
-                'c_idx': number().ge(0).le(255).integer().required(),
-                'distance_from': mixed().one_of(['INDEX', 'VALUE']).required(),
-                'distance': number().ge(100).le(300000).integer().required(),
-                'reticle_idx': number().ge(0).le(255).integer().required(),
-                'zoom': number().ge(0).le(255).integer().required(),
+                'c_idx': required(number().ge(0).le(255).integer()),
+                'distance_from': required(mixed().one_of(['INDEX', 'VALUE'])),
+                'distance': required(number().ge(100).le(300000).integer()),
+                'reticle_idx': required(number().ge(0).le(255).integer()),
+                'zoom': required(number().ge(0).le(255).integer()),
             })
         ).min(4),
 
         # rifle
-        'sc_height': number().ge(-5000).le(5000).integer().required(),
-        'r_twist': number().ge(0).le(10000).integer().required(),
-        'twist_dir': mixed().one_of(['RIGHT', 'LEFT']).required(),
+        'sc_height': required(number().ge(-5000).le(5000).integer()),
+        'r_twist': required(number().ge(0).le(10000).integer()),
+        'twist_dir': required(mixed().one_of(['RIGHT', 'LEFT'])),
 
         # cartridge
-        'c_muzzle_velocity': number().ge(100).le(30000).integer().required(),
-        'c_zero_temperature': number().ge(-100).le(100).integer().required(),
-        'c_t_coeff': number().ge(0).le(5000).integer().required(),
+        'c_muzzle_velocity': required(number().ge(100).le(30000).integer()),
+        'c_zero_temperature': required(number().ge(-100).le(100).integer()),
+        'c_t_coeff': required(number().ge(0).le(5000).integer()),
 
         # zero params
-        'c_zero_distance_idx': number().ge(0).le(255).integer().required(),
-        'c_zero_air_temperature': number().ge(-100).le(100).integer().required(),
-        'c_zero_air_pressure': number().ge(3000).le(15000).integer().required(),
-        'c_zero_air_humidity': number().ge(0).le(100).integer().required(),
-        'c_zero_w_pitch': number().ge(-90).le(90).integer().required(),
-        'c_zero_p_temperature': number().ge(-100).le(100).integer().required(),
+        'c_zero_distance_idx': required(number().ge(0).le(255).integer()),
+        'c_zero_air_temperature': required(number().ge(-100).le(100).integer()),
+        'c_zero_air_pressure': required(number().ge(3000).le(15000).integer()),
+        'c_zero_air_humidity': required(number().ge(0).le(100).integer()),
+        'c_zero_w_pitch': required(number().ge(-90).le(90).integer()),
+        'c_zero_p_temperature': required(number().ge(-100).le(100).integer()),
 
         # bullet
-        'b_diameter': number().ge(1).le(50000).integer().required(),
-        'b_weight': number().ge(10).le(65535).integer().required(),
-        'b_length': number().ge(1).le(200000).integer().required(),
+        'b_diameter': required(number().ge(1).le(50000).integer()),
+        'b_weight': required(number().ge(10).le(65535).integer()),
+        'b_length': required(number().ge(1).le(200000).integer()),
 
         # drag model
-        'bc_type': mixed().one_of(['G1', 'G7', 'CUSTOM']).required(),
-        'coef_rows': array().min(1).max(200).required()
+        'bc_type': required(mixed().one_of(['G1', 'G7', 'CUSTOM'])),
+        'coef_rows': required(array().min(1).max(200))
     }),
 })
 
@@ -86,23 +86,26 @@ def _coef_rows_mv_validator(rows):
             invalid_value=rows
         )
 
-_coef_rows_std_schema = array().of(
-    mapping().shape({
-        'bc_cd': number().ge(0).le(10000).integer(),
-        'mv': number().ge(0).le(30000).integer(),
-    })
-).min(1).max(5).required(
-    'For G1 or G7, coefRows must contain between 1 and 5 items'
-).test(_coef_rows_mv_validator)
 
-_coef_rows_custom_schema = array().of(
-    mapping().shape({
-        'bc_cd': number().ge(0).le(10000).integer(),
-        'mv': number().ge(0).le(10000).integer()
-    })
-).min(1).max(200).required(
+_coef_rows_std_schema = required(
+    array().of(
+        mapping().shape({
+            'bc_cd': number().ge(0).le(10000).integer(),
+            'mv': number().ge(0).le(30000).integer(),
+        })
+    ).min(1).max(5).test(_coef_rows_mv_validator),
+    'For G1 or G7, coefRows must contain between 1 and 5 items'
+)
+
+_coef_rows_custom_schema = required(
+    array().of(
+        mapping().shape({
+            'bc_cd': number().ge(0).le(10000).integer(),
+            'mv': number().ge(0).le(10000).integer()
+        })
+    ).min(1).max(200).test(_coef_rows_mv_validator),
     'For CUSTOM, coefRows must contain between 1 and 200 items'
-).test(_coef_rows_mv_validator)
+)
 
 
 def validate(payload: profedit_pb2.Payload, fail_fast: bool = False):
@@ -135,5 +138,3 @@ def validate(payload: profedit_pb2.Payload, fail_fast: bool = False):
             "invalid payload",
             "invalid payload"
         ), path="~", errors=errors, invalid_value=payload)
-
-
